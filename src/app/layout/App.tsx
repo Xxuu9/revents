e14214e6583f9084d8848b36ac90ed74d2e7114f
Xxuu9 <tengxxu@gmail.com>
2025-01-11 14:16:10 -0500

    add routing

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,36 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Container } from "semantic-ui-react";
-import EventDashboard from "../../features/events/dashboard/EventDashboard";
 import Navbar from "./nav/NavBar";
-import { useState } from "react";
-import { AppEvent } from "../types/event";
+import { Outlet, useLocation } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
 
 function App() {
-  const [formOpen, setFormOpen] = useState(false);
-  const [selectedEvent, setSelectedEvent] = useState<AppEvent | null>(null);
-
-  function handleSelectEvent(event: AppEvent | null) {
-    setSelectedEvent(event);
-    setFormOpen(true);
-  }
-
-  function handleCreateFormOpen() {
-    setSelectedEvent(null);
-    setFormOpen(true);
-  }
-
+  const location = useLocation();
   return (
     <>
-      <h1>Welcome</h1>
-      <Navbar setFormOpen={handleCreateFormOpen} />
-      <Container className="main">
-        <EventDashboard
-          formOpen={formOpen}
-          setFormOpen={setFormOpen}
-          selectedEvent={selectedEvent}
-          selectEvent={handleSelectEvent}
-        />
-      </Container>
+      {location.pathname === "/" ? (
+        <HomePage />
+      ) : (
+        <>
+          <Navbar />
+          <Container className="main">
+            <Outlet />
+          </Container>
+        </>
+      )}
     </>
   );
 }
--- src/features/events/dashboard/EventListItem.tsx
+++ src/features/events/dashboard/EventListItem.tsx
@@ -10,18 +10,13 @@ import {
 } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
 import { AppEvent } from "../../../app/types/event";
+import { Link } from "react-router-dom";
 
 type Props = {
   event: AppEvent;
-  selectEvent: (event: AppEvent) => void;
-  deleteEvent: (eventId: string) => void;
 };
 
-export default function EventListItem({
-  event,
-  selectEvent,
-  deleteEvent,
-}: Props) {
+export default function EventListItem({ event }: Props) {
   return (
     <SegmentGroup>
       <Segment>
@@ -56,17 +51,13 @@ export default function EventListItem({
       </Segment>
       <Segment clearing>
         <span>{event.description}</span>
+        <Button color="red" floated="right" content="Delete" />
         <Button
-          color="red"
-          floated="right"
-          content="Delete"
-          onClick={() => deleteEvent(event.id)}
-        />
-        <Button
+          as={Link}
+          to={`/events/${event.id}`}
           color="teal"
           floated="right"
           content="View"
-          onClick={() => selectEvent(event)}
         />
       </Segment>
     </SegmentGroup>
